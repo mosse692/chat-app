82d4527ea28b84cc5b3230d4df2a45e86ceb92b1
mosse692 <40247330+mosse692@users.noreply.github.com>
2024-08-15 15:14:58 +0200

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
       const {username, password, phoneNumber, AvatarURL} = form
 
-      const URL = 'http://localhost:5000/auth'
+      const URL = '/auth'
 
       const {data: {token, userId, hashedPassword, fullName}} = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`,{
         username, password, fullName: form.fullName, phoneNumber, AvatarURL
